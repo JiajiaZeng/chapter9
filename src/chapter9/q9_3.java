69bea8900ba8aab9be4a68d39bc026b8b2729ba7
JiajiaZeng <3034177851@qq.com>
2021-12-01 18:10:52 +0800

    chapter9

---
+++ src/chapter9/q9_3.java
@@ -0,0 +1,26 @@
+package chapter9;
+
+import java.util.Date;
+
+public class q9_3 {
+	Date date=new Date();
+	
+	public void setDate(int n) {
+		date.setTime(n);
+	}
+	
+	public void getDate() {
+		System.out.println(date.toString());
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Date date=new Date();
+		for(long i=10000;i<1e12;i=i*10) {
+			date.setTime(i);
+			System.out.println(date.toString());
+			
+		}
+	}
+
+}
